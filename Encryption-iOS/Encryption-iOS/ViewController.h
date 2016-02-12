65aabf1f5b338e257476c3fa0b1e877615342851
Danil <voitenko.cr@gmail.com>
2016-02-12 12:22:50 +0200

    Start Encryption-iOS

---
+++ Encryption-iOS/Encryption-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Encryption-iOS
+//
+//  Created by Danil on 12.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
