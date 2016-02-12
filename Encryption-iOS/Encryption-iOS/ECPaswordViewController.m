81fea420e57a00df506053d91a1e959353d414ee
Danil <voitenko.cr@gmail.com>
2016-02-12 13:07:06 +0200

    add constraints for logInScreen;

---
+++ Encryption-iOS/Encryption-iOS/ECPaswordViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  Encryption-iOS
+//
+//  Created by Danil on 12.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import "ECPaswordViewController.h"
+
+@interface ECPaswordViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *centerYConstraint;
+@property (assign, nonatomic) CGFloat centerYConstantValue;
+
+@end
+
+@implementation ECPaswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.centerYConstantValue = self.centerYConstraint.constant;
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+
+}
+
+-(void)viewDidDisappear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)enterButtonDidPressed:(id)sender {
+    
+}
+- (IBAction)dismissKeyboard:(id)sender {
+    [self.passwordField resignFirstResponder];
+}
+
+- (void)keyboardWasShown:(NSNotification *)notification {
+    NSDictionary *userInfo = notification.userInfo;
+    CGSize kbSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    self.centerYConstraint.constant =- kbSize.height /2;
+}
+
+- (void)keyboardWillBeHidden:(NSNotification *)notification {
+    self.centerYConstraint.constant = self.centerYConstantValue;
+}
+@end
