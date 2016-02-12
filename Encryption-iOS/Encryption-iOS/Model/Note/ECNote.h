b17f3a54795eafdc14a917418394a588f05635f0
Danil <voitenko.cr@gmail.com>
2016-02-12 13:48:09 +0200

    create note class;

---
+++ Encryption-iOS/Encryption-iOS/Model/Note/ECNote.h
@@ -0,0 +1,15 @@
+//
+//  ECNote.h
+//  Encryption-iOS
+//
+//  Created by Danil on 12.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECNote : NSObject
+
+@property (strong, nonatomic) NSDate *creationDate;
+@property (strong, nonatomic) NSString *noteText;
+@end
---
+++ Encryption-iOS/Encryption-iOS/Model/Note/ECNote.m
@@ -0,0 +1,13 @@
+//
+//  ECNote.m
+//  Encryption-iOS
+//
+//  Created by Danil on 12.02.16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import "ECNote.h"
+
+@implementation ECNote
+
+@end
